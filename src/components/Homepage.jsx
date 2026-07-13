3b9d4e61f52172564918b4a0851c0db368beda61
sajana-46 <sajanaskuttiyil@gmail.com>
2026-07-13 19:23:01 +0530

    homepage completed

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+
+const Homepage = () => {
+  return (
+    <div>
+        
+        <center>
+
+        
+        <h1>Vehicle App</h1>
+        
+        <h2>WELCOME</h2>
+        </center>
+    </div>
+  )
+}
+
+export default Homepage
